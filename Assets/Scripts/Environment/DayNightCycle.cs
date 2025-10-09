bbca62bff2455e655a7658ee7f807a0662b919e4
EddySantiagoS <eddy.samudio@uao.edu.co>
2025-10-08 21:03:34 -0500

    Clico día/noche con dos luces

---
+++ Assets/Scripts/Environment/DayNightCycle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public Light sunLight;
+    public float rotationspeed;
+
+    void Update()
+    {
+        sunLight.transform.Rotate(Vector3.right, rotationspeed * Time.deltaTime);
+    }
+}
